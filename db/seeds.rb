6ce80332a0044511faef1b7cf2e069cbb86794ee
Micaela Moss <mcmoss1991@gmail.com>
2020-01-07 17:05:46 -0800

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.find_by(username: "Mando")
-user.destroy
+User.destroy_all
 
-demoUser = User.create({email: "DinDjarin@mandalorian.com", username: "Mando", password: "password"})
+demoUser = User.create!({email: "DinDjarin@mandalorian.com", username: "Mando", password: "password"})

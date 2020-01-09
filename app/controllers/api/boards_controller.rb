2f8204de380e7e538a7a53f4f855db27c540c860
Micaela Moss <mcmoss1991@gmail.com>
2020-01-08 17:38:31 -0800

    Update boards controller

--- app/controllers/api/boards_controller.rb
+++ app/controllers/api/boards_controller.rb
@@ -1,5 +1,7 @@
 class Api::BoardsController < ApplicationController
 
+    before_action :require_logged_in
+
     def create
         @board = Board.new(board_params)
         @board.creator_id = current_user.id
@@ -11,7 +13,7 @@ class Api::BoardsController < ApplicationController
     end
 
     def show
-        @board = Board.find(params[:id])
+        @board = current_user.boards.find(params[:id])
         if @board
             render "api/boards/show"
         else
@@ -20,7 +22,26 @@ class Api::BoardsController < ApplicationController
     end
 
     def index
-        @boards
+        @boards = current_user.boards
+        render "api/boards/index"
+    end
+
+    def update
+        @board = current_user.boards.find(params[:id])
+        if @board.update_attributes(board_params)
+            render "api/boards/show"
+        else
+            render json: @board.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy
+        @board = current_user.boards.find(params[:id])
+        if @board.destroy
+            render {}
+        else
+            render plain: "That board does not exist."
+        end
     end
 
 

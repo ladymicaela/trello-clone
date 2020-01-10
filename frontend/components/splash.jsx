32437ab003c48e1e140248ce96740c1fe330eb4c
Micaela Moss <mcmoss1991@gmail.com>
2020-01-10 11:58:34 -0800

    Style splash page

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -7,7 +7,10 @@ const Splash = () => {
                 <h1>R2DToDo lets you work more collaboratively and get more done.</h1>
                 <p>R2DToDo's boards, lists, and cards enable you to organize and prioritize your projects in a fun, flexible, and rewarding way.</p>
             </div>
-            
+            <div className="hero-image">
+                <img src={window.r2d2URL}/>
+                <img src={window.c3poURL}/>
+            </div>
         </div>
     )
 };

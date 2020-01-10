e9c0c09870b206aa841fddb0445f36922ea1b58c
Micaela Moss <mcmoss1991@gmail.com>
2020-01-10 12:44:08 -0800

    Add styling to footer

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <div className="footer">
-            <a href="https://www.linkedin.com/in/micaela-moss-686b3b75" target="_blank"><i class=" fab fa-linkedin"></i></a>
-            <a href="https://github.com/ladymicaela" target="_blank"><i class="fab fa-github-square"></i></a>
+            <a href="https://www.linkedin.com/in/micaela-moss-686b3b75" target="_blank"><i className=" fab fa-linkedin"></i></a>
+            <a href="https://github.com/ladymicaela" target="_blank"><i className="fab fa-github-square"></i></a>
         </div>
     )
 };

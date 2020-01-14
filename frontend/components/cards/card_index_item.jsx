751638b4cbd846e82fa5d8c97030fbf3e622660f
Micaela Moss <mcmoss1991@gmail.com>
2020-01-14 08:30:04 -0800

    Tweak styling

--- frontend/components/cards/card_index_item.jsx
+++ frontend/components/cards/card_index_item.jsx
@@ -28,12 +28,13 @@ class CardIndexItem extends React.Component {
 
         return(
             <div className="card-index-item">
-                <a onClick={() => this.props.openModalWithItem('show-card', this.props.card)}>{this.props.card.title}</a>
+                <a onClick={() => this.props.openModalWithItem('show-card', this.props.card)}>{this.props.card.title}
                 {/* <a onClick={this.openShow}>{this.props.card.title}</a> */}
-                <div className="card-alert-icons">
-                    <i className={description}></i>
-                    <i className={dueDate}></i>
-                </div>
+                    <div className="card-alert-icons">
+                        <i className={description}></i>
+                        <i className={dueDate}></i>
+                    </div>
+                </a>
             </div>
         )
     }
